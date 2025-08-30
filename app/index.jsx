83ea42fa5682dc139734424894223bef39ab161a
Victoria Choi <121906670+vicchois@users.noreply.github.com>
2025-08-30 13:06:45 -0700

    Adding Post Home and Component

--- app/index.jsx
+++ app/index.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 import { useRouter } from 'expo-router';
 
-export default function HomeScreen() {
+export default function Landing() {
   const router = useRouter();
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to Blueprint!</Text>
       <Button title="Go to Example Page" onPress={() => router.push('/example/example')} />
+      <Button title="Go to Post Home Page" onPress={() => router.push('/post/home')} />
     </View>
   );
 }
